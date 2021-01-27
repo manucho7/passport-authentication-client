b3d541c6a011f199d1d89aef150393cbb001744d
manucho <manuel.blanco77@gmail.com>
2021-01-27 13:01:24 -0300

    Installing @types for react-router-dom + definition of routes in App.tsx + /Pages definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Homepage from './Pages/Homepage';
+import AdminPage from './Pages/AdminPage';
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <div>
-      Hola Mundo
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Homepage} />
+        <Route path='/admin' component={AdminPage} />
+        <Route path='/login' component={Login} />
+        <Route path='/profile' component={Profile} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Homepage() {
   return (
     <div>
-      
+      Home Page
     </div>
   )
 }
