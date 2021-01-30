6b515bf533192e20f516e858776a1b065ac12288
manucho <manuel.blanco77@gmail.com>
2021-01-30 11:57:13 -0300

    Creating Register page

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 export default function Register() {
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const register = () => {
+    axios.post("http://localhost:4000/register", {
+      username,
+      password
+    }, {
+      withCredentials: true
+    }).then(res => {
+      if (res.data === "Succesfully Authenticated") {
+        window.location.href = "/login";
+      }
+    })
+  }
+
   return (
     <div>
-      Register 
+      <h1>Register</h1>
+      <input type="text" placeholder="username" onChange={ e => setUsername(e.target.value) }/>
+      <input type="text" placeholder="password" onChange={ e => setPassword(e.target.value) }/>
+      <button onClick={ register }>Register</button>
     </div>
   )
 }
